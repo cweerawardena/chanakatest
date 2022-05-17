17f63ba5f438bbb71c46fd20f81ed32908e36736
Chanaka Weerawardena <cweerawardena@virtusa.com>
2022-05-17 17:31:52 +0530

    [ABC-123] #comment initial commit

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string str = "welcome@to-qa-with-experts#.com";
+            string replacestr = Regex.Replace(str, "[^a-zA-Z0-9_]+", " ");
+            Console.WriteLine(replacestr);
+            Console.ReadLine();
+        }
+    }
+}
